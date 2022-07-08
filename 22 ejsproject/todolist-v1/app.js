238bcc0e3fda06899b72b47477fc31e686201cae
KSX <kushagra17800@gmail.com>
2022-07-08 15:42:33 +0530

    7/7/22

--- 22 ejsproject/todolist-v1/app.js
+++ 22 ejsproject/todolist-v1/app.js
@@ -7,8 +7,17 @@ const app = express();
 
 app.get("/", function(req, res){
     // res.send("Hello");
-    var today = 
+    var today = new Date();
+    var currentday = today.toLocaleTimeString();
+    res.send(currentday)
+    // if(today.getDay()===0 || today.getDay()===6 ){
+    //     res.send("<h1>weekday</h1>");
+    // }
+    // else{
+    //     res.send("<h1>workday</h1>");
+    // }
 }); 
 
 app.listen(3000, function(){
-    console.log("server stated
+    console.log("server stated on port 3000");
+});
