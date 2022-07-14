bebb7698c9c3a75e90f77a90dab3c331964599f2
KSX <kushagra17800@gmail.com>
2022-07-14 20:42:38 +0530

    14/07/22

--- 22 ejsproject/todolist-v1/app.js
+++ 22 ejsproject/todolist-v1/app.js
@@ -5,19 +5,26 @@ const bodyParser = require("body-parser")
 
 const app = express();
 
-app.get("/", function(req, res){
-    // res.send("Hello");
-    var today = new Date();
-    var currentday = today.toLocaleTimeString();
-    res.send(currentday)
-    // if(today.getDay()===0 || today.getDay()===6 ){
-    //     res.send("<h1>weekday</h1>");
-    // }
-    // else{
-    //     res.send("<h1>workday</h1>");
-    // }
-}); 
+app.set('view engine', 'ejs');
 
-app.listen(3000, function(){
+app.get("/", function (req, res) {
+    var today = new Date();
+    
+    var options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    }
+    
+    // var day = today.toLocaleDateString("en-US", options); 
+    
+    var day = today.toLocaleDateString("hi-IN", options); 
+
+    res.render("list", { 
+        kindofday: day 
+    });
+});
+
+app.listen(3000, function () {
     console.log("server stated on port 3000");
 });
